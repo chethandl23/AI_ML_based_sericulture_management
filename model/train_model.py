41f5824a569407bf2830782622a7bdb98e06b24f
ChethanDL <Chethan.22ai010@sode-edu.in>
2025-11-07 15:38:10 +0530

    second commit

---
+++ model/train_model.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.svm import SVR
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+import joblib
+
+def train_model
